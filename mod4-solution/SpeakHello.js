47fc9ca2e9bd657db80097d2099d1ae6dd983687
Divyansh Agarwal <67423340+divyanshagarwal100@users.noreply.github.com>
2020-08-18 12:55:19 +0530

    Update SpeakHello.js
    update new hello

--- mod4-solution/SpeakHello.js
+++ mod4-solution/SpeakHello.js
@@ -1,4 +1,4 @@
- // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
+// STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
 // See Lecture 52, part 2
 
 
@@ -6,7 +6,7 @@
 // the "speak" method and which you will expose to the global context
 // See Lecture 52, part 1
 (function (window) {
-    'use strict';
+    
     var helloSpeaker = {};
     helloSpeaker.name = "Yaakov";
     helloSpeaker.name = "John";
@@ -17,16 +17,19 @@
     helloSpeaker.name = "Larry";
     helloSpeaker.name = "Paula";
     helloSpeaker.name = "Laura";
+    helloSpeaker.name = "Jim";
     var speakWord = "Hello";
+    
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
     
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-    helloSpeaker.sayHello = function speak(names) {
-        console.log(speakWord + " " + helloSpeaker.names);
-    };
+    helloSpeaker.speak = function (name) {
+   console.log(speakWord + " " + name);
+    }
+    window.helloSpeaker = helloSpeaker;
     
 })(window);
 
