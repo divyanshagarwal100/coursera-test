b5782d2c0130765d0baefaf9ca55266232974721
Divyansh Agarwal <67423340+divyanshagarwal100@users.noreply.github.com>
2020-08-18 12:54:20 +0530

    Upadate script.js
    new update js

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -31,16 +31,16 @@ WARNING!!! WARNING!!!
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
-    'use strict';
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-    
+(function (window) {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
 // STEP 10: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Loop over the names array and say either 'Hello' or "Good Bye"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-    for (var i = 0; i < names.length; i++) {
+for (var i = 0; i < names.length; i++) {
 
   // STEP 11: (NOTHING TO DO. ALREADY DONE FOR YOU)
   // Retrieve the first letter of the current name in the loop.
@@ -60,7 +60,5 @@ WARNING!!! WARNING!!!
     byeSpeaker.speak(names[i]);
   } else {
     helloSpeaker.speak(names[i]);
-  }
-}
-
-})();
+  }}
+})(window);
